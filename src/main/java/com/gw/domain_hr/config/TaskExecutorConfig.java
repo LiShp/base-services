c078d19bd12362a5eddab3b28b9189acbd791778
ze zoujialiang@163.comgit config --global user.name zoujialiang@163.comgit config --global user.name ze <zoujialiang@163.com>
2019-12-03 14:22:36 +0800

    增加线程池配置文件

---
+++ src/main/java/com/gw/domain_hr/config/TaskExecutorConfig.java
@@ -0,0 +1,28 @@
+package com.gw.domain_hr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class TaskExecutorConfig implements AsyncConfigurer {
+
+
+    private static final int CORE_POOL_SIZE = 2;//线程池维护线程的最少数量，核心线程池
+    private static final int MAX_POOL_SIZE = 10;//线程池维护线程的最大数量
+    private final static int KEEP_ALIVE_TIME = 0;//线程池维护线程所允许的空闲时间
+    private static final int QUEUE_CAPACITY = 50;//线程池所使用的缓冲队列大小
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(CORE_POOL_SIZE);
+        taskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
+        taskExecutor.setKeepAliveSeconds(KEEP_ALIVE_TIME);
+        taskExecutor.setQueueCapacity(QUEUE_CAPACITY);
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
